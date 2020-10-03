9786ea3d21d03f26fff8fd1cbd6657f1d8b48199
Pratik Patil <connect@pratikpatil.net>
2020-10-03 10:29:55 +0530

    Added HTTP Interceptors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment';
 @Component({
@@ -9,8 +10,12 @@ export class AppComponent {
   title = 'start-app';
   environmentName = '';
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     this.environmentName = environment.name;
+  }
+
+  onClick() {
+    this.httpClient.get("http://dummyhost.xyz/").subscribe(data => { }, err => { });
   }
 
 }
